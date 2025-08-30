a8c7430325bc2f7e6a7d569de6e8c519935e92ea
AbdulazizRus <coderdim@gmail.com>
2025-08-30 18:05:40 +0500

    files auth apikeys route add

--- backend/src/controllers/apikey.controller.ts
+++ backend/src/controllers/apikey.controller.ts
@@ -1,11 +1,60 @@
 import type { Request, Response } from "express";
+import { HTTPSTATUS } from "../config/http.config";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
-import { createApiKeySchema } from "../validators/apikey.validator";
+import {
+  createApiKeyService,
+  deleteApiKeyService,
+  getAllApiKeysService,
+} from "../services/apikey.service";
+import {
+  createApiKeySchema,
+  deleteApiKeySchema,
+} from "../validators/apikey.validator";
 
 export const createApiKeyController = asyncHandler(
   async (req: Request, res: Response) => {
     const userId = req.user?._id;
 
     const { name } = createApiKeySchema.parse(req.body);
+
+    const { rawKey } = await createApiKeyService(userId, name);
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "API key created successfully",
+      key: rawKey,
+    });
+  }
+);
+
+export const getAllApiKeysController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id;
+
+    const pagination = {
+      pageSize: parseInt(req.query.pageSize as string) || 20,
+      pageNumber: parseInt(req.query.pageNumber as string) || 1,
+    };
+
+    const results = await getAllApiKeysService(userId, pagination);
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "API keys retrieved successfully",
+      ...results,
+    });
+  }
+);
+
+export const deleteApiKeyController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id;
+
+    const { id } = deleteApiKeySchema.parse(req.body);
+
+    const result = await deleteApiKeyService(userId, id);
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "API key deleted successfully",
+      data: result,
+    });
   }
 );
--- backend/src/routes/internal/files.route.ts
+++ backend/src/routes/internal/files.route.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import { multiUpload } from "../../config/multer.config";
-import { uploadFilesViaWebController } from "../../controllers/files.controller";
+import {
+  deleteFilesController,
+  downloadFilesController,
+  getAllFilesController,
+  uploadFilesViaWebController,
+} from "../../controllers/files.controller";
 import { CheckStorageAvailability } from "../../middlewares/check-storage.middleware";
 
 const filesRoutes = Router();
@@ -12,8 +17,8 @@ filesRoutes.post(
   uploadFilesViaWebController
 );
 
-filesRoutes.post("/download");
-filesRoutes.get("/all");
-filesRoutes.delete("/bulk-delete");
+filesRoutes.post("/download", downloadFilesController);
+filesRoutes.get("/all", getAllFilesController);
+filesRoutes.delete("/bulk-delete", deleteFilesController);
 
 export default filesRoutes;
