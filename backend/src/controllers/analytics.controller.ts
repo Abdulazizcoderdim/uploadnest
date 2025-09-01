e571861ab72c10285abc28630fbc5bc004d7d851
AbdulazizRus <coderdim@gmail.com>
2025-09-01 08:47:03 +0500

    analytixts add

---
+++ backend/src/controllers/analytics.controller.ts
@@ -0,0 +1,24 @@
+import type { Request, Response } from "express";
+import { HTTPSTATUS } from "../config/http.config";
+import { asyncHandler } from "../middlewares/asyncHandler.middleware";
+import { getUserAnalyticsWithChartService } from "../services/analytics.service";
+
+export const getUserAnalyticsWithChartController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id;
+
+    const { from, to } = req.query;
+
+    const filter = {
+      dateFrom: from ? new Date(from as string) : undefined,
+      dateTo: to ? new Date(to as string) : undefined,
+    };
+
+    const result = await getUserAnalyticsWithChartService(userId, filter);
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "User analytics retrieved successfully",
+      ...result,
+    });
+  }
+);
--- backend/src/routes/internal/analytics.route.ts
+++ backend/src/routes/internal/analytics.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { getUserAnalyticsWithChartController } from "../../controllers/analytics.controller";
 
 const analyticsRoutes = Router();
 
-analyticsRoutes.get("/user");
+analyticsRoutes.get("/user", getUserAnalyticsWithChartController);
 
 export default analyticsRoutes;
